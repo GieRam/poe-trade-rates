08bf0dd5b7293c6b37139a81138e853811c36171
Gintaute Ezerskyte <gintaute.ez@gmail.com>
2016-11-02 21:38:53 +0200

    Adds datepicker restrictions

--- src/main/webapp/assets/helper/graphForm.js
+++ src/main/webapp/assets/helper/graphForm.js
@@ -1,29 +1,44 @@
 var graphForm = (function() {
-    function getRequestType() {
-      return $('select option:selected').val();  
+  $.ajax({
+    url: "http://" + window.location.host + "/trade-info/earliest",
+    success: function(result) {
+      $(function () {
+        $('.datetimepicker')
+        .datetimepicker({
+          format: 'YYYY-MM-DD',
+          minDate: result,
+          maxDate: new Date
+        })
+      });
     }
-    $("select").change(function() {
-      var showDateFrom = $("#from").show();
-      var showDateTo = $("#to").show();
-      var requestType = getRequestType();
-      switch (requestType) { 
-        case "day":
-          showDateTo.toggle();
-          break;
-        case "week":
-        case "month":
-          showDateTo.toggle();
-          showDateFrom.toggle();
-          break;
-        default:
-          showDateFrom;
-          showDateTo;
-          break;
-      }
-    });
-    return {
-      requestType: getRequestType,
-      startDate: function(){return $("#dayFrom").val();},
-      endDate: function(){return $("#dayTo").val();}
+  }); 
+
+  function getRequestType() {
+    return $('select option:selected').val();  
+  }
+
+  $("select").change(function() {
+    var showDateFrom = $("#from").show();
+    var showDateTo = $("#to").show();
+    var requestType = getRequestType();
+    switch (requestType) { 
+      case "day":
+        showDateTo.toggle();
+        break;
+      case "week":
+      case "month":
+        showDateTo.toggle();
+        showDateFrom.toggle();
+        break;
+      default:
+        showDateFrom;
+        showDateTo;
+        break;
     }
+  });
+  return {
+    requestType: getRequestType,
+    startDate: function(){return $("#dayFrom").val();},
+    endDate: function(){return $("#dayTo").val();}
+  }
   })();
